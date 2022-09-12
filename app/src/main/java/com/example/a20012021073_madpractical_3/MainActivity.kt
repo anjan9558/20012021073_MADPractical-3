004d38767cc9bbc69df1922e7fd05ebcd0813044
Keyoori55 <keyurirabadia@gmail.com>
2022-09-12 12:40:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a20012021073_madpractical_3/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.a20012021073_madpractical_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.example.a20012021073_madpractical_3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
